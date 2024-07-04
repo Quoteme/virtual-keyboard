38e6780fd3248289ad8474d625f06119e477d460
Luca Leon Happel <lucahappel99@gmx.de>
2024-07-04 15:15:47 +0200

    add: automatically create missing .venv

--- shell.nix
+++ shell.nix
@@ -10,6 +10,13 @@ mkShell {
   NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
   shellHook = ''
     export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+    # check if the venv is already created
+    if [ ! -d .venv ]; then
+      python3 -m venv .venv
+      source .venv/bin/activate
+      pip install --upgrade pip
+      pip install -r requirements.txt
+    fi
     source .venv/bin/activate
     trap "exit 0" EXIT
   '';
