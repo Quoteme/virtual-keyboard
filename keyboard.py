1c4cf9366eaa1d771180eb486b7ba061d13740c1
Luca Leon Happel <lucahappel99@gmx.de>
2024-07-04 18:23:46 +0200

    add: buttons

--- keyboard.py
+++ keyboard.py
@@ -1,20 +1,34 @@
 import dearpygui.dearpygui as dpg
 
-
-def save_callback():
-    print("Save Clicked")
-
+kes = [
+    ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "BACKSPACE"],
+    ["TAB", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\"],
+    ["CAPS", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "ENTER"],
+    ["SHIFT", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "SHIFT"],
+    ["CTRL", "WIN", "ALT", "SPACE", "ALT", "FN", "MENU", "CTRL"],
+]
 
 dpg.create_context()
-dpg.create_viewport()
+
+with dpg.window(
+    tag="Primary Window",
+):
+    dpg.add_text("Hello, world")
+    with dpg.table(header_row=False):
+        for row in kes:
+            with dpg.table_row():
+                for key in row:
+                    dpg.add_table_cell(label=key)
+                    dpg.add_button(label=key, width=50, height=50)
+
+dpg.create_viewport(
+    title="Custom Title",
+    width=600,
+    height=200,
+    decorated=False,
+)
 dpg.setup_dearpygui()
-
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello world")
-    dpg.add_button(label="Save", callback=save_callback)
-    dpg.add_input_text(label="string")
-    dpg.add_slider_float(label="float")
-
 dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
 dpg.start_dearpygui()
 dpg.destroy_context()
